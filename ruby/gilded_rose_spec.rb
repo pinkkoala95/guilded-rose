38d0a0f60f0852e94eaac6326b5b6520616366fb
pinkkoala95 <emma.keeley@madetech.com>
2022-12-12 15:54:03 +0000

    initial refactor with Megan

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -6,7 +6,19 @@ describe GildedRose do
     it "does not change the name" do
       items = [Item.new("foo", 0, 0)]
       GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "fixme"
+      expect(items[0].name).to eq "foo"
+    end
+
+    xit "does subtract 1 from quality if the name is foo " do
+      items = [Item.new("foo", 0, 5)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 4
+    end
+
+    it "does adds 2 to quality if the name is foo " do
+      items = [Item.new("foo", 0, 5)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 4
     end
   end
 
